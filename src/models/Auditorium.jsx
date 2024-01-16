b871a7e8c221d1933e9baeabd397f9fad17b4d83
Francis Yang <fy50167@gmail.com>
2024-01-16 15:47:23 -0800

    react-spring

---
+++ src/models/Auditorium.jsx
@@ -0,0 +1,276 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import auditoriumScene from '../assets/3d/auditorium.glb';
+import { a } from '@react-spring/three';
+
+export default function Auditorium() {
+    const { nodes, materials } = useGLTF(
+        "/Concert_Hall__Amphitheater_VR_Nov.glb"
+    );
+    
+    return (
+        
+        <a.group {...props} dispose={null}>
+        <group scale={100}>
+            <group rotation={[-Math.PI / 2, 0, 0]} scale={0.128}>
+            <group rotation={[Math.PI / 2, 0, 0]}>
+                <group position={[0, 91.29, 5.353]}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.polySurface4054_lambert64_0.geometry}
+                    material={materials.lambert64}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.polySurface4054_lambert64_0_1.geometry}
+                    material={materials.lambert64}
+                />
+                </group>
+                <group position={[0, -24.771, 87.956]}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.polySurface4058_lambert64_0.geometry}
+                    material={materials.lambert64}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.polySurface4058_lambert64_0_1.geometry}
+                    material={materials.lambert64}
+                />
+                </group>
+                <group scale={100}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.chair_wood_lambert55_0.geometry}
+                    material={materials.lambert55}
+                    position={[0.023, 0.152, 2.174]}
+                    rotation={[0, Math.PI / 2, 0]}
+                    scale={0.006}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.chair_leather_lambert54_0.geometry}
+                    material={materials.lambert54}
+                    position={[0.023, 0.152, 2.174]}
+                    rotation={[0, Math.PI / 2, 0]}
+                    scale={0.006}
+                />
+                </group>
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface174_lambert63_0.geometry}
+                material={materials.lambert63}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface174_lambert63_0_1.geometry}
+                material={materials.lambert63}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface218_lambert64_0.geometry}
+                material={materials.lambert64}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface218_lambert64_0_1.geometry}
+                material={materials.lambert64}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface228_lambert64_0.geometry}
+                material={materials.lambert64}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface228_lambert64_0_1.geometry}
+                material={materials.lambert64}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface228_lambert64_0_2.geometry}
+                material={materials.lambert64}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4053_lambert63_0.geometry}
+                material={materials.lambert63}
+                position={[0, 91.29, 5.353]}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.pCylinder109_lambert74_0.geometry}
+                material={materials.lambert74}
+                position={[0, 0, -0.879]}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4055_lambert63_0.geometry}
+                material={materials.lambert63}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4056_lambert64_0.geometry}
+                material={materials.lambert64}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4056_lambert64_0_1.geometry}
+                material={materials.lambert64}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes._Mesh6_lambert59_0.geometry}
+                material={materials.lambert59}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.pCube34_lambert75_0.geometry}
+                material={materials.lambert75}
+                scale={[0.989, 1, 1]}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4100_lambert67_0.geometry}
+                material={materials.lambert67}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4324_lambert80_0.geometry}
+                material={materials.lambert80}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface1_lambert49_0.geometry}
+                material={materials.lambert49}
+                position={[0, 0, -5.02]}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface169_lambert50_0.geometry}
+                material={materials.lambert50}
+                position={[0, 0, -5.02]}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface175_lambert51_0.geometry}
+                material={materials.lambert51}
+                position={[0, 0, -5.02]}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface182_lambert57_0.geometry}
+                material={materials.lambert57}
+                position={[0, 0, -5.02]}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface305_lambert56_0.geometry}
+                material={materials.lambert56}
+                position={[0, 0, -5.02]}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Pino_polySurface174_lambert58_0.geometry}
+                material={materials.lambert58}
+                position={[-10.35, 15.196, 221.676]}
+                rotation={[0, 1.528, 0]}
+                scale={1.547}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4194_lambert76_0.geometry}
+                material={materials.lambert76}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4195_lambert63_0.geometry}
+                material={materials.lambert63}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4195_lambert63_0_1.geometry}
+                material={materials.lambert63}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4201_lambert73_0.geometry}
+                material={materials.lambert73}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4294_lambert81_0.geometry}
+                material={materials.lambert81}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4203_lambert72_0.geometry}
+                material={materials.lambert72}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4325_lambert82_0.geometry}
+                material={materials.lambert82}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4326_lambert83_0.geometry}
+                material={materials.lambert83}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4295_lambert77_0.geometry}
+                material={materials.lambert77}
+                />
+                <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.polySurface4296_lambert79_0.geometry}
+                material={materials.lambert79}
+                />
+            </group>
+            </group>
+        </group>
+        </group>
+
+        useGLTF.preload("/Concert_Hall__Amphitheater_VR_Nov.glb");
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,26 +3,24 @@ import { Suspense } from 'react';
 
 export default function Home() {
     return (
-        <>
-            <section classname = 'w-full h-screen relative'>
-                <div classname = 'absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-                    Hi! This is my site!
-                </div>
+        <section classname = 'w-full h-screen relative'>
+            <div classname = 'absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+                Hi! This is my site!
+            </div>
 
-                <Canvas 
-                    classname = 'w-full h-screen bg-transparent'
-                    camera = {{near: 0.1, far: 1000}}
-                >
-                    <Suspense fallback = {<Loader />}>
-                        <directionalLight />
-                        <ambientLight />
-                        <pointLight />
-                        <spotLight />
-                        <hemisphereLight />
-                    </Suspense>
+            <Canvas 
+                classname = 'w-full h-screen bg-transparent'
+                camera = {{near: 0.1, far: 1000}}
+            >
+                <Suspense fallback = {<Loader />}>
+                    <directionalLight />
+                    <ambientLight />
+                    <pointLight />
+                    <spotLight />
+                    <hemisphereLight />
+                </Suspense>
 
-                </Canvas>
-            </section>
-        </>
+            </Canvas>
+        </section>
     )
 }
