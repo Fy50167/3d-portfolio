107cd1e565656150cded18b51a89a1f4efa7c056
Francis Yang <fy50167@gmail.com>
2024-01-19 15:38:28 -0800

    postprocessing

--- src/models/Auditorium.jsx
+++ src/models/Auditorium.jsx
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
-import { useFrame, useThree } from "@react-three/fiber";
+import { useGLTF, MeshReflectorMaterial } from "@react-three/drei";
 import auditoriumScene from '../assets/3d/auditorium.glb';
 import { a } from '@react-spring/three';
 
@@ -267,7 +266,25 @@ export default function Auditorium(props) {
                 />
             </group>
             </group>
+            
         </group>
+            <mesh position-y={-0.48} rotation-x={-Math.PI / 2}>
+                <planeGeometry args={[100, 100]} />
+                <MeshReflectorMaterial
+                blur={[100, 100]}
+                resolution={2048}
+                mixBlur={1}
+                mixStrength={10}
+                roughness={1}
+                depthScale={1}
+                opacity={0.5}
+                transparent
+                minDepthThreshold={0.4}
+                maxDepthThreshold={1.4}
+                color="#333"
+                metalness={0.5}
+                />
+            </mesh>
         </a.group>
     )
 }
