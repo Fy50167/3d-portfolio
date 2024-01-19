ddfb293d24135aae53c88fc70ac640c4ef4958ee
Francis Yang <fy50167@gmail.com>
2024-01-18 20:25:01 -0800

    setting up helper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,18 +8,23 @@ import { Suspense } from 'react';
 import Auditorium from '../models/Auditorium';
 import Loader from '../components/Loader';
 
-const OrbitCameraControls = () => {
-    const {camera, gl} = useThree();
-    useEffect(() => {
-        const controls = new OrbitControls(camera, gl.domElement);
-    
-        controls.minDistance = 0;
-        return () => {
-            controls.dispose();
-        }
-    }, [camera, gl]);
-    return null
-}
+
+
+function LightScene() {
+    const { x, tx, y, ty, z, tz, intensity } = useControls({ x: { value: 1, min: 0, max: 20 }, y: { value: 1, min: 0, max: 3000 }, z: { value: 1, min: 0, max: 7000 }, intensity: { value: 0.5, min: 0, max: 5 }});
+   const dirLight = useRef();
+
+    // Use the directional light helper
+    useHelper(dirLight, DirectionalLightHelper, 500, 'red');
+
+    return (
+        <>
+            <ambientLight intensity = {0.1}/>
+            <directionalLight target = {[tx, ty, tz]} position = {[x, y, z]} intensity = {intensity} color = '#efddb4' ref={dirLight}/>
+            <hemisphereLight color = '#FFD700' groundColor = {'#000000'} intensity = {0.1} />
+        </>
+    )
+};
 
 export default function Home() {
     const adjustModel = () => {
@@ -38,20 +43,14 @@ export default function Home() {
 
     const [auditoriumScale, auditoriumPosition, auditoriumRotation] = adjustModel(); 
 
-    const dirLight = useRef<DirectionalLight>(null);
-    useHelper(dirLight, DirectionalLightHelper, 1, "red");
-
-    const { x, y, z } = useControls({ x: { value: 1, min: 0, max: 20 }, y: { value: 1, min: 0, max: 20 }, z: { value: 1, min: 0, max: 20 }});
-
-
     return (
         <section className = 'w-full h-screen relative'>
             <Canvas 
-                className = 'w-full h-screen bg-transparent'
-                camera = {{near: 0.1, far: 20000, position: [50, 2100, -1850], fov: [50],  rotation: [Math.PI / 8, Math.PI, 0]}}
+                className="w-full h-screen bg-transparent"
+                camera={{ near: 0.1, far: 20000, position: [50, 2100, -1850], fov: [50], rotation: [Math.PI / 8, Math.PI, 0]}}
             >
                 <Suspense fallback = {<Loader />}>
-                    <FirstPersonControls 
+                    {/* <FirstPersonControls 
                         activeLook
                         autoForward
                         enabled
@@ -63,13 +62,8 @@ export default function Home() {
                         movementSpeed={0.5}
                         verticalMax={3.141592653589793}
                         verticalMin={0}
-                    />
-                    <ambientLight intensity = {0.1}/>
-                    <directionalLight position = {[x, y, z]} intensity = {0.4} ref = {dirLight}/>
-                    <hemisphereLight color = '#FFD700' groundColor = {'#000000'} intensity = {0.1} />
-                    
-
-                    
+                    /> */}
+                    <LightScene />
                     <Auditorium 
                         position = {auditoriumPosition}
                         scale = {auditoriumScale}
