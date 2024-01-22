09176f192ddc45e85f0501f4b68c9a5e12446fff
Francis Yang <fy50167@gmail.com>
2024-01-21 21:14:44 -0800

    useFrame

--- src/components/SecionText.jsx
+++ src/components/SecionText.jsx
@@ -1,13 +1,20 @@
 import { Text } from '@react-three/drei';
 import { useState, useRef } from 'react';
-import { useThree } from '@react-three/fiber';
+import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
 export default function SectionText(props) {
+    const markerRef = useRef();
+    useFrame((state) => {
+        state.camera.position.lerp({ x: 50, y: 25, z: 50 });
+        state.camera.lookAt(0, 0, 0);
+        state.camera.updateProjectionMatrix;
+    });
+
     return (
         <>
             <Text
-                position={[-50, 1500, 2500]}
+                position={[-1500, 500, 1000]}
                 font={'fonts/JuliusSansOne-Regular.ttf'}
                 color='#b99b4c'
                 fontSize={150}
@@ -15,6 +22,7 @@ export default function SectionText(props) {
                 rotation={[0, Math.PI, 0]}
                 letterSpacing={0.02}
                 textAlign='center'
+                ref={markerRef}
             >
                 FRANCIS YANG{'\n'}WEB DEVELOPER
             </Text>
