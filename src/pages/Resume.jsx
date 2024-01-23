95e9421fcf3250d59642264d09257f07b7db8f56
Francis Yang <fy50167@gmail.com>
2024-01-23 05:22:03 -0800

    resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,11 @@
+import RESUME from '../assets/files/Resume.pdf';
+
 export default function Resume() {
+    document.title = 'Francis Yang - Resumé';
+
     return (
         <>
-            <div className = 'text-red-800'>
-                Resume
-            </div>
+            <div className='text-red-800'>Resume</div>
         </>
-    )
+    );
 }
