880674084447a5c7bdced9e15935f11f99e83f5d
Francis Yang <fy50167@gmail.com>
2024-01-23 18:02:46 -0800

    render content based on redux variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Home from './pages/Home';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import { useSelector } from 'react-redux';
 
 export default function App() {
+    const viewContent = useSelector((state) => state.content.value);
+
     return (
         <>
             <main>
                 <Header />
                 <Home />
-                <Outlet />
+                {viewContent && <Outlet />}
             </main>
         </>
     );
